3b986fbb16816a7996ec71e11336a1e867088cf4
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-06-23 15:04:07 -0500

    updated spline elements and css styling

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import '../App.css'
-import {Button, Container} from 'react-bootstrap'
+import {Button, Card, Container} from 'react-bootstrap'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Header from './Header'
 import Spline from '@splinetool/react-spline';
@@ -9,8 +9,8 @@ import {motion, useScroll, useTransform} from 'framer-motion'
 
 
 function Intro(){
-    let {scrollYProgress} = useScroll()
-    let y = useTransform(scrollYProgress, [0,0.5], ["0%", "-100%"])
+    let {scrollY} = useScroll()
+    let y = useTransform(scrollY, [0,1000], ["0%", "-100%"])
 
     return(
         <motion.div className='intro-page' style={{ y }}>
@@ -24,7 +24,7 @@ function Intro(){
                         <p className='my-3'> Delivering powerful user experiences across diverse disciplines.</p>
                     </Container>
                 </Container>
-                <Spline className='macbook'scene="https://prod.spline.design/kBPVgA-a5v7IAjt0/scene.splinecode" />
+                <Spline className='macbook'scene="https://prod.spline.design/b8L-D0RCKd9YOiT6/scene.splinecode" />
             </div>
             
         </motion.div>
@@ -32,22 +32,28 @@ function Intro(){
 }
 
 function Overview(){
-    let {scrollYProgress} = useScroll()
-    let y = useTransform(scrollYProgress, [0.5,1], ["0%", "-500%"])
+    let {scrollY} = useScroll()
+    let y = useTransform(scrollY, [500,1000], ["0%", "-100%"])
     return(
         <motion.div className='skills-page text-light' style={{ y }}>
             <Container>
-                <h1 style={{fontSize:`100px`}}>Skills</h1>
-                <h1 style={{fontSize:`150px`}}>Ali Siddiqui</h1>
-                <h5>Full Stack Developer | Indie Game Developer</h5>
-                <h1 style={{fontSize:`100px`}}>Skills</h1>
-                <h1 style={{fontSize:`150px`}}>Ali Siddiqui</h1>
-                <h5>Full Stack Developer | Indie Game Developer</h5>
-                <h1 style={{fontSize:`100px`}}>Skills</h1>
-                <h1 style={{fontSize:`150px`}}>Ali Siddiqui</h1>
-                <h5>Full Stack Developer | Indie Game Developer</h5>
+                <h1 style={{fontSize:`150px`, marginRight:`100rem`}} className='pb-5 mb-5'>Skills</h1>
+                <Container className='row'>
+                    <Card style={{width: `20rem`}} className='py-4'>
+                        <h1>Front End</h1>
+                        <Card.Text>JavaScript, React, Tailwind, and many more..</Card.Text>
+                    </Card>
+                    <Card style={{width: `20rem`}} className='py-4'>
+                        <Card.Title>Video-Game Development</Card.Title>
+                        <Card.Text>Using in-demand skills such as React and JS.</Card.Text>
+                    </Card>
+                    <Card style={{width: `20rem`}} className='py-4'>
+                        <Card.Title>Back End</Card.Title>
+                        <Card.Text>Django, Express, NodeJS, and Monogoose.</Card.Text>
+                    </Card>
+                </Container>
             </Container>
-
+            
         </motion.div>
     )
 }
